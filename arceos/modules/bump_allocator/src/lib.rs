05ed16fcbdde09c3a6e250d522844b8e28db66e9
Sasuke0723 <szy2159707599@gmail.com>
2025-12-14 19:31:42 +0800

    finish alt_alloc

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,21 +16,43 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    base : usize,
+    size : usize,
+    b_pos: usize,
+    p_pos: usize,
+    bytes_used: usize,
+    pages_used: usize,
+    count: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            base: 0,
+            size: 0,
+            b_pos: 0,
+            p_pos: 0,
+            bytes_used: 0,
+            pages_used: 0,
+            count: 0,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.base = start;
+        self.size = size;
+        self.b_pos = 0;
+        self.p_pos = size;
+        self.bytes_used = 0;
+        self.pages_used = 0;
+        self.count = 0;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
-        todo!()
+        Err(allocator::AllocError::NoMemory)
     }
 }
 
@@ -39,11 +61,30 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let align = layout.align();
+        let size = layout.size();
+
+        let aligned_b_pos = (self.b_pos + align - 1) & !(align - 1);
+        if aligned_b_pos + size > self.p_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+
+        let ptr = unsafe { (self.base + aligned_b_pos) as *mut u8 };
+        self.b_pos = aligned_b_pos + size;
+        self.bytes_used += size;
+        self.count += 1;
+
+        Ok(core::ptr::NonNull::new(ptr).unwrap())
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        let size = layout.size();
+        self.bytes_used -= size;
+        self.count -= 1;
+        if self.count == 0 {
+            self.b_pos = 0;
+            self.bytes_used = 0;
+        }
     }
 
     fn total_bytes(&self) -> usize {
